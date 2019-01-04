fa80d444df05eda9b27129f61be10b16c9e9c81b
Matt Macy <mmacy@FreeBSD.org>
2019-01-04 09:36:49 -0500

    Add FreeBSD support to ZoL
    - Import FreeBSD specific parts
    - Update tests to work on FreeBSD
    
    Signed-off-by: Matt Macy <mmacy@FreeBSD.org>
    Co-authored-by: Ryan Moeller <ryan@iXsystems.com>
    Signed-off-by: Ryan Moeller <ryan@iXsystems.com>
    
    Requires-builders: amazon2, debian10, freebsd12, freebsd13, style
    
    TEST_ZTEST_SKIP="yes"

--- lib/libzfs/libzfs_util.c
+++ lib/libzfs/libzfs_util.c
@@ -1834,36 +1834,6 @@ zfs_version_userland(char *version, int len)
 }
 
 /*
- * Fill given version buffer with zfs kernel version read from ZFS_SYSFS_DIR
- * Returns 0 on success, and -1 on error (with errno set)
- */
-int
-zfs_version_kernel(char *version, int len)
-{
-	int _errno;
-	int fd;
-	int rlen;
-
-	if ((fd = open(ZFS_SYSFS_DIR "/version", O_RDONLY)) == -1)
-		return (-1);
-
-	if ((rlen = read(fd, version, len)) == -1) {
-		version[0] = '\0';
-		_errno = errno;
-		(void) close(fd);
-		errno = _errno;
-		return (-1);
-	}
-
-	version[rlen-1] = '\0';  /* discard '\n' */
-
-	if (close(fd) == -1)
-		return (-1);
-
-	return (0);
-}
-
-/*
  * Prints both zfs userland and kernel versions
  * Returns 0 on success, and -1 on error (with errno set)
  */
