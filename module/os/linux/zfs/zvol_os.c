3de47ac9553093594b4e3264ce6daaa4d238f3f9
Matthew Macy <mmacy@FreeBSD.org>
2020-05-12 23:22:32 -0700

    style

--- module/os/linux/zfs/zvol_os.c
+++ module/os/linux/zfs/zvol_os.c
@@ -326,7 +326,7 @@ zvol_strategy_dmu_done(dmu_ctx_t *dc)
 	if (dc->dc_resid_init == dc->dc_size)
 		len = dc->dc_completed_size;
 	else
-	    len = dc->dc_size;
+		len = dc->dc_size;
 
 	err = dc->dc_err;
 	reader = !!(dc->dc_flags & DMU_CTX_FLAG_READ);
